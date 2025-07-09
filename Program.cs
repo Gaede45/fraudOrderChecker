9a37be5ad4a91b96013ab9bcbaca092b90571c38
Gabriel Augusto Tomasoni Gaede <110550147+Gaede45@users.noreply.github.com>
2025-07-09 11:42:31 -0300

    Add project files

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿string[] idOrders = ["B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"];
+
+// Detect fraudulent orders whose ID starts with 'B'
+foreach (string id in idOrders)
+{
+    if (id.StartsWith("B"))
+    {
+        Console.WriteLine($"(!) Fraudulent order found: {id}");
+    }
+}
